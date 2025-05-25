9f9e60e1672df9f0260f7d3d64a89910347c7d54
MoRsHeD <mdmorshed0187@gmail.com>
2025-05-25 17:55:43 +0600

    created three table such as rangers,species & sightings

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,2 +1,31 @@
 -- Active: 1747663712588@@127.0.0.1@5432@conservation_db
 CREATE DATABASE conservation_db;
+
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    region VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(50) NOT NULL,
+    scientific_name VARCHAR(50) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INT REFERENCES rangers(ranger_id),
+    species_id INT REFERENCES species(species_id),
+    sighting_name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    notes VARCHAR(25)
+);
+
+
+
+SELECT * FROM rangers;
+SELECT * FROM species;
+SELECT * FROM sightings;
